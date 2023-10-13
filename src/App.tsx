29afd432044ad774ec2e0e9d7c1312b888e80916
renangton <puccho0427@gmail.com>
2023-10-13 23:03:40 +0900

    Firebaseへのデプロイ用のファイルを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hello World!</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
