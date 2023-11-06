45fb8f7b4f238ef84e0d8911696c6f9e98561bf0
renangton <puccho0427@gmail.com>
2023-11-07 03:29:50 +0900

    モーダルのサイズ及び文字サイズを変更、モーダル外の背景色を変更、モーダル外クリックでモーダルを閉じれるように変更

--- src/ResultModal.jsx
+++ src/ResultModal.jsx
@@ -3,19 +3,36 @@ import React from "react";
 import { css } from "@emotion/react";
 
 function ResultModal({ isOpen, message, onRequestClose }) {
+  const modalBackgroundClick = (e) => {
+    if (e.target === e.currentTarget) {
+      onRequestClose();
+    }
+  };
+
   return (
     isOpen && (
-      <div css={modalContainer}>
-        <div css={modalContent}>
-          <p css={modalMessage}>{message}</p>
-          <button css={modalCloseButton} onClick={onRequestClose}>
-            閉じる
-          </button>
+      <div css={modalBackdrop} onClick={modalBackgroundClick}>
+        <div css={modalContainer}>
+          <div css={modalContent}>
+            <p css={modalMessage}>{message}</p>
+            <button css={modalCloseButton} onClick={onRequestClose}>
+              閉じる
+            </button>
+          </div>
         </div>
       </div>
     )
   );
 }
+
+const modalBackdrop = css`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+`;
 
 const modalContainer = css`
   position: fixed;
@@ -25,6 +42,8 @@ const modalContainer = css`
   background: #ffffff;
   border: 1px solid #dcdcdc;
   border-radius: 10px;
+  width: 200px;
+  height: 120px;
   padding: 30px;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
   z-index: 999;
@@ -37,8 +56,8 @@ const modalContent = css`
 const modalMessage = css`
   color: #333;
   font-weight: bold;
-  font-size: 24px;
-  padding-bottom: 20px;
+  font-size: 20px;
+  padding-bottom: 10px;
 `;
 
 const modalCloseButton = css`
